81659df4fe68135e45891681a857959327b42333
bedmonds13 <005275435@coyote.csusb.edu>
2021-06-25 12:39:33 -0700

    Restructuring

--- C++/OpenGL/OpenGL/ConsoleApplication1/Game.h
+++ C++/OpenGL/OpenGL/ConsoleApplication1/Game.h
@@ -4,41 +4,42 @@
 class Game
 {
 	int SCREEN_WIDTH = 1000,SCREEN_HEIGHT = 1000 ;
-	float old_t;
 public:
+	long long old_t;
 	Game()
 	{
 		gluOrtho2D(-SCREEN_WIDTH, SCREEN_WIDTH, -SCREEN_HEIGHT, SCREEN_HEIGHT);
-		GameWindow();
 		old_t = glutGet(GLUT_ELAPSED_TIME);
 	}
 	void GameWindow()
 	{
+		glutInitDisplayMode(GLUT_SINGLE);
 		glutInitWindowSize(SCREEN_WIDTH, SCREEN_HEIGHT);
 		glutCreateWindow("Game");
 		Init();
 	}
-	void Start()
-	{
-		glutDisplayFunc(Render);
-		glutMainLoop();
-	}
 	void Update()
 	{
+		old_t = glutGet(GLUT_ELAPSED_TIME);
+		std::cout << (old_t / 1000.0f) << std::endl;
+
+		draw();
+	}
+
+	void draw()
+	{
+
 	}
 	
-	static void Render()
-	{
-		//Update();
-		//glFlush;
-		glutSwapBuffers();
-		
-	}
 	void Init()
 	{
 		glClearColor(1.0f, 0.3f, 0.1f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 	};
+	~Game()
+	{
+		
+	}
 
 	
 
--- C++/OpenGL/OpenGL/ConsoleApplication1/main.cpp
+++ C++/OpenGL/OpenGL/ConsoleApplication1/main.cpp
@@ -2,13 +2,11 @@
 #include "Ball.h";
 #include "Game.h"
 #include <iostream>
-
-
+#include "stb_image.h"
 
 using namespace std;
 
-const int SCREEN_WIDTH = 50;
-const int SCREEN_HEIGHT = 50;
+/*
 int x = 0;
 int y = 0;
 int vx = 0;
@@ -18,9 +16,8 @@ const int BALLSPEED = 2;
 
 Ball game_ball = Ball(30, 10);
 int ball_radius = 4;
-
-float old_t = 0;
-
+*/
+Game* NewGame = new Game();
 
 
 void Window()
@@ -30,35 +27,48 @@ void Window()
 
 void keyboard(unsigned char key, int mouseX , int mouseY )
 {
-	switch (key) {
-		case 'w':
-			y++;
-			break;
-		case 's':
-			y--;
-			break;
-		case 'a':
-			x--;
-			break;
-		case 'd':
-			x++;
-			break;
-		default:
-			break;
-	}
-	glutPostRedisplay();
+	if (key == 27)
+		exit(0);
+	
 }
-int RandomDirection()
+int RandomNumberGenerator(int from, int to)
 {
-		srand(time(0));
-		int num = rand() % 3 + (-1);
+		srand(time(NULL));
+		int num = rand() % to + from;
 		return num;
 }
+
+void display()
+{
+	NewGame->Update();
+
+
+	glFinish();
+	glutPostRedisplay();
+}
+
+
+void init()
+{
+	NewGame->old_t  = glutGet(GLUT_ELAPSED_TIME);
+}
+
+
+int main(int argc, char* argv[])
+{
+	
+	init();
+	glutInit(&argc, argv);
+	NewGame->GameWindow();
+	glutDisplayFunc(display);
+	glutKeyboardFunc(keyboard);
+	glutMainLoop();
+	return 0;
+}
+/*
 void Initialize()
 {
-	glPointSize(2.0);
-	gluOrtho2D(-SCREEN_WIDTH, SCREEN_WIDTH, -SCREEN_HEIGHT, SCREEN_HEIGHT);
-	glClearColor(0.2,0.3,0.1,0.2);
+	
 	while (game_ball.get_velocity_x() == 0 && game_ball.get_velocity_y() == 0)
 	{
 		game_ball.set_velocity_x(BALLSPEED * RandomDirection());
@@ -69,8 +79,6 @@ void Initialize()
 
 void Update(int)
 {
-	//time_elapsed();
-
 	if (game_ball.get_position_x() >= SCREEN_WIDTH ||
 		game_ball.get_position_x() <= -SCREEN_WIDTH )
 		game_ball.set_velocity_x(-(game_ball.get_velocity_x()));
@@ -88,6 +96,7 @@ void Update(int)
 	game_ball.move();
 	glutPostRedisplay();
 	glutTimerFunc(1000/30, Update, 0);
+	
 }
 
 
@@ -101,29 +110,4 @@ void drawbox(int x, int y)
 		glVertex2i(1+x,-1+y);
 	glEnd();
 }
-
-void display()
-{
-	glClear(GL_COLOR_BUFFER_BIT);
-	drawbox(x,y);
-	game_ball.draw(20, ball_radius);
-	glFlush();
-
-}
-
-void init()
-{
-	old_t = glutGet(GLUT_ELAPSED_TIME);
-}
-
-int main(int argc, char* argv[])
-{
-	init();
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_SINGLE);
-	Game* NewGame = new Game();
-	NewGame->Start();
-	
-
-	return 0;
-}
+*/
